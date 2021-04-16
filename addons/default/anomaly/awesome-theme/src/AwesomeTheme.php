66aca0495d66884a589f040aac6ff31ea792382b
denismwangi <denniske992@gmail.com>
2021-04-16 12:53:43 +0300

    pages

---
+++ addons/default/anomaly/awesome-theme/src/AwesomeTheme.php
@@ -0,0 +1,10 @@
+<?php namespace Anomaly\AwesomeTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+class AwesomeTheme extends Theme
+{
+
+}
+
+
---
+++ addons/default/anomaly/awesome-theme/tests/Feature/AwesomeThemeTest.php
@@ -0,0 +1,10 @@
+<?php namespace Anomaly\AwesomeTheme\Test\Feature;
+
+class AwesomeThemeTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
